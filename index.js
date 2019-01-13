dffcdc1b0c333fd0339f95c31c7cdb4b7f61e625
matmikus <mateusz.mikolajczak@yahoo.com>
2019-01-13 15:47:48 +0100

    poprawka w komunikacie na serwerze

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('W tym miejscu powstaje serwis www.rozgrywki.online'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`rozgrywki.online app listening on port ${port}!`))
