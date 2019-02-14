29863b480fd6bea0753487e3d6e442ff4ff28ea2
matmikus <mateusz.mikolajczak@yahoo.com>
2019-02-14 20:51:03 +0100

    syntax refactor for eslint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { Router } = require('express')
 const cookieParser = require('cookie-parser')
 
 // Dev local import process.env from .env file
--- api/routes/get_competitions.js
+++ api/routes/get_competitions.js
@@ -3,7 +3,11 @@ const router = require('express')()
 
 router.get('/get_competitions', (req, res) => {
   db.databaseClient().query('select * from competitions;', (err, dbResult) => {
-    res.send(dbResult.rows)
+    if (err) {
+      res.sendStatus(500)
+    } else {
+      res.send(dbResult.rows)
+    }
   })
 })
 
